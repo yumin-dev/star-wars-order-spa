a1d2c4bf89caf16c8224f6f02d01f718ce771d14
yumin-dev <yumin.li.2005@gmail.com>
2020-04-21 05:18:29 -0400

    add router

--- src/app.js
+++ src/app.js
@@ -1,14 +1,27 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from 'react-router-dom';
 
-const App = ({ title }) => (
-  <div>
-    <div>{title}</div>
-    <div>Random text displayed</div>
-  </div>
+
+const Home = () => (
+  <h1> Hello this is react-router home..! </h1>);
+const Address = () => (
+  <h1> This is address page for react-router example !</h1>);
+const NotFound = () => (
+  <h1>404.. This page is not found!</h1>);
+
+const App = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/star-wars/watch-order" component={Address} />
+      <Route component={NotFound} />
+    </Switch>
+  </Router>
 );
-export default App;
 
-App.propTypes = {
-  title: PropTypes.string.isRequired,
-};
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -35,5 +35,6 @@ module.exports = {
   devServer: {
     contentBase: `${outputFolder}`,
     hot: true,
+    historyApiFallback: true,
   },
 };
