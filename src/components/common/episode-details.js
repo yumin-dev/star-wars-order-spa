99a6ff351cd12b4fb2ef0ccffb74d20cdf95f2ed
yumin-dev <yumin.li.2005@gmail.com>
2020-04-22 23:05:56 -0400

    format layout

--- src/components/common/episode-details.js
+++ src/components/common/episode-details.js
@@ -2,40 +2,35 @@ import * as React from 'react';
 import FavoriteIcon from '../../assets/favorite.svg';
 
 const EpisodeDetails = ({ episode, favorite, favoriteHandler }) => (
-  <div style={{ display: 'flex', borderTop: '1px solid #999', padding: '12px 24px' }}>
+  <div style={{
+    display: 'flex',
+    borderTop: '1px solid #999',
+    padding: '12px 24px',
+    fontFamily: 'Verdana, Arial',
+  }}
+  >
     <div style={{ paddingRight: '36px' }}>
       <img src={episode.coverUrl} alt="Cover" width={200} />
     </div>
     <div style={{ flexGrow: 4, paddingRight: '36px' }}>
       <div>
-        <h4>
-Title:
-          {episode.title}
-        </h4>
+        <h3>{episode.title}</h3>
       </div>
       <div>
-        <h4>
-Release Date:
-          {episode.released}
-        </h4>
+        <b>Director:</b>
+        <span style={{ paddingLeft: '12px' }}>{episode.directors}</span>
       </div>
       <div>
-        <h4>
-Director:
-          {episode.directors}
-        </h4>
+        <b>Actors:</b>
+        <span style={{ paddingLeft: '12px' }}>{episode.actors}</span>
       </div>
       <div>
-        <h4>
-Actors:
-          {episode.actors}
-        </h4>
+        <b>Release Date:</b>
+        <span style={{ paddingLeft: '12px' }}>{episode.released}</span>
       </div>
       <div>
-        <h4>
-Plot:
-          {episode.plot}
-        </h4>
+        <b>Plot:</b>
+        <span style={{ paddingLeft: '12px' }}>{episode.plot}</span>
       </div>
     </div>
     <div>
